f97ff9eccefa9ee96ed6b5c347443887c48ce704
Kipras <kipras@anaweb.lt>
2021-01-31 16:51:17 +0200

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <b-nav>
+      <b-nav-item disabled><b>Quizaroo</b></b-nav-item>
+      <b-nav-item disabled>Counter {{ numCorrect }}/{{ totalAnswers }}</b-nav-item>
+    </b-nav>
+  </div>
+</template>
+
+<script>
+export default {
+name: "Header",
+  props: [
+      'numCorrect',
+      'totalAnswers'
+  ]
+}
+</script>
+
+<style scoped>
+
+</style>
